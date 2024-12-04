b16004e5564ec457010836670a0f3ee9ebdfa55a
Aditya <ak.adityakumar421@gmail.com>
2024-12-04 13:06:11 +0530

    change db

--- app/database/database.py
+++ app/database/database.py
@@ -4,7 +4,7 @@ from app.config.settings import settings
 MONGO_DB_URL = settings.MONGO_DB_URL
 DATABASE_NAME = settings.DATABASE_NAME
 
-client = AsyncIOMotorClient(MONGO_DB_URL)
+client = AsyncIOMotorClient(MONGO_DB_URL,tls=True)
 db = client[DATABASE_NAME]
 
 # Collection reference 
